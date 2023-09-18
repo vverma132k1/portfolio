f494274d23d89a517195106343e192da605079d7
vverma132k1 <vverma132k1@gmail.com>
2023-09-18 09:13:32 +0530

    making changes

--- script.js
+++ script.js
@@ -47,3 +47,55 @@ async function copyToClipboard() {
     // alert("Failed to copy text: " + err);
   }
 }
+
+
+var words = [
+  "Web Developer", 
+  "Problem Solver",  
+  "frontend Engineer",
+  "backend Engineer"
+],
+part,
+i = 0,
+offset = 0,
+len = words.length,
+forwards = true,
+skip_count = 0,
+skip_delay = 15,
+speed = 70;
+
+var wordflick = function () {
+setInterval(function () {
+  if (forwards) {
+    if (offset >= words[i].length) {
+      ++skip_count;
+      if (skip_count == skip_delay) {
+        forwards = false;
+        skip_count = 0;
+      }
+    }
+  } else {
+    if (offset == 0) {
+      forwards = true;
+      i++;
+      offset = 0;
+      if (i >= len) {
+        i = 0;
+      }
+    }
+  }
+  part = words[i].substr(0, offset);
+  if (skip_count == 0) {
+    if (forwards) {
+      offset++;
+    } else {
+      offset--;
+    }
+  }
+  $('.word').text("I'm a " + part);
+}, speed);
+};
+
+$(document).ready(function () {
+wordflick();
+});
